90de52e15d194435d3c55b52b3c44f9cbb02ede5
omullick <omkarmullick@gmail.com>
2022-09-17 16:35:55 +0530

    Initial Commit

---
+++ src/main/java/com/software/omkarm/example/MicroServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.software.omkarm.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/software/omkarm/example/MicroServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.omkarm.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
